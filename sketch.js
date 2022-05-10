2e35691486b188d8d27bec9ef6ec365221d18f2b
Kevin Nguyen <7575701@isd535.org>
2022-05-09 23:03:57 -0500

    honda logo created

---
+++ sketch.js
@@ -0,0 +1,42 @@
+const square = [];
+const totalFrames = 240;
+let counter = 0;
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  
+
+  const percent = float(counter % totalFrames) / totalFrames;
+  render(percent);
+  counter++;
+}
+
+function render(percent) {
+  background(0);
+  translate(width / 2, height / 2);
+  stroke(255, 0, 100);
+  strokeWeight(4);
+  fill(255, 199, 44);
+  
+  beginShape();
+  for (let v of square) {
+    const a = map(percent, 0, 1, 0, TWO_PI * 0);
+    const r = map(sin(a), -1, 1, height / 80, height / 40);
+    vertex(r * v.x, r * v.y);
+    
+  }
+  endShape();
+
+  if (percent < 0.5) {
+    const a = map(percent, 0, 0.5, 0, TWO_PI);
+    const x = 16 * pow(sin(a), 1);
+    const y = -(13 * cos(a) - 4 * cos(3 * a) - 5 * cos(3 * a) - cos(2 * a));
+    square.push(createVector(x, y));
+  } else {
+    square.splice(0, 1);
+  }
+
+}
