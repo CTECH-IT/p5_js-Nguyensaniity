a127d11f58451b40b1ac9b1982b4270c6fc0cbc3
Kevin Nguyen <7575701@isd535.org>
2022-05-13 10:45:55 -0500

    speed

--- sketch.js
+++ sketch.js
@@ -27,15 +27,15 @@ function render(percent) {
   
   beginShape();
   for (let v of square) {
-    const a = map(percent, 0, 1, 1, TWO_PI * 0);
-    const r = map(sin(a), -1, 4, height / 80, height / 40);
+    const a = map(percent, 6, .1, .2, TWO_PI * 5);
+    const r = map(sin(a), -1, 4, height / 60, height / 40);
     vertex(r * v.x, r * v.y);
     
   }
   endShape();
 
   if (percent < 0.5) {
-    const a = map(percent, 0, 0.5, 0, TWO_PI);
+    const a = map(percent, 0, 1, 1, TWO_PI * 6);
     const x = 16 * pow(sin(a), 1);
     const y = -(13 * cos(a) - 4 * cos(3 * a) - 5 * cos(3 * a) - cos(2 * a));
     square.push(createVector(x, y));
